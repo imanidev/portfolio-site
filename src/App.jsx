3d20296b4fa9d9bfc3ff3a8b3ad44bc4df3f59a1
imani.devs <imaniroberts.dev@gmail.com>
2023-05-23 07:43:08 -0400

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import '../src/components/Header'
-import '../src/components/Navbar'
-import '../src/components/Footer'
-import '../src/pages/Home'
-import '../src/pages/About'
-import '../src/pages/Contact'
-import '../src/pages/Projects'
-import '../src/pages/Resume'
-
-
+import Navbar from '../src/components/Navbar';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Home from '../src/pages/Home';
+import Projects from '../src/pages/Projects';
+import About from '../src/pages/About';
+import Contact from '../src/pages/Contact';
+import Resume from '../src/pages/Resume';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes>
+      <Footer />
     </div>
   );
 }
