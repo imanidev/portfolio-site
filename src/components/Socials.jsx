cb087d7c221ecdde5ad4b20f4fd105f4b4b6d8fb
imani.devs <imaniroberts.dev@gmail.com>
2023-05-23 18:59:18 -0400

    created links to social media accs

---
+++ src/components/Socials.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Socials = () => {
+    return (
+        <div className='socials'>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/imaniroberts" target="_blank">Linkedin</a>
+                </li>
+                <li>
+                    <a href="https://www.github.com/imanidev" target="_blank">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.twitter.com/imaniverse" target="_blank">Twitter</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Socials;
