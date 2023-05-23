8a6a0d5890407e17a07ecdb1340d24223fccc249
imani.devs <imaniroberts.dev@gmail.com>
2023-05-23 10:11:30 -0400

    added typewriter effect

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,34 @@ import Projects from '../src/pages/Projects';
 import About from '../src/pages/About';
 import Contact from '../src/pages/Contact';
 import Resume from '../src/pages/Resume';
+import TypeWriter from 'typewriter-effect';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
       <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/resume" element={<Resume />} />
-            </Routes>
+      <h1>
+        <TypeWriter className="typewriter"
+          onInit={(typewriter) => {
+            typewriter.typeString('Hello, I am a Full Stack Web Developer').start();
+          }}
+        />
+      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+
+
