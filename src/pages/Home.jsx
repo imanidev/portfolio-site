304fd9f98cbe08748ede24bd8538904acfe0a129
imani.devs <imaniroberts.dev@gmail.com>
2023-05-23 15:50:42 -0400

    added typing effect in home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import TypingEffect from '../utils/TypingEffect';
 
 
 const Home = () => {
     return (
         <div>
-       
+            <TypingEffect />
+            <div className='intro-text'>
+                <p>
+                    With a focus on the MERN stack, I combine creativity, empathy, and a healthy dose of caffeine to solve problems.
+                    <br />
+                    <br />
+                    From ideation to execution, I prioritize intuitive interfaces and seamless interactions to ensure everyone can easily navigate in the digital world.
+                </p>
+            </div>
 
-        </div>
+        </div >
     );
 };
 
