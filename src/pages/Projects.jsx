334ebd62292ea5240f834abc5397d6f81c44daa2
imani.devs <imaniroberts.dev@gmail.com>
2023-05-23 21:13:39 -0400

    added github and live demo buttons

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,13 +6,27 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className='dictionary-container'>
                 <img className='dict-gif' src='./src/assets/dict-app.gif' alt='dictionary app' />
-<p className='dict-desc'>Technologies: HTML, CSS, JavaS </p>
+                <p className='dict-desc'>Technologies: React, Vite, Bootstrap, API</p>
+                <br />
+                <button className='demo-btn'>
+                    <a href='https://the-simple-dictionary.netlify.app/' target='_blank'>Live Demo</a>
+                </button>
+                <button className='github-btn'>
+                    <a href='https://github.com/imanidev/dictionary-app>' target='_blank'>Github</a>
+                </button>
             </div>
             <br />
             <br />
             <div className='pong-container'>
                 <img className='pong-gif' src='./src/assets/pong-app.gif' alt='pong app' />
                 <p className='pong-desc'>Technologies: </p>
+                <br />
+                <button className='demo-btn'>
+                    <a href='https://imanidev.github.io/pong_game/' target='_blank'>Live Demo</a>
+                </button>
+                <button className='github-btn'>
+                    <a href='https://github.com/imanidev/pong_game' target='_blank'>Github</a>
+                </button>
             </div>
         </div>
     );
