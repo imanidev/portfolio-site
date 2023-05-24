0d03a4fa7c33f865848c704649cb1f2bb7e336cc
imani.devs <imaniroberts.dev@gmail.com>
2023-05-24 16:40:28 -0400

    added file

---
+++ src/utils/showNavBackground.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+
+const ShowNavBackground = () => {
+  useEffect(() => {
+    const navUl = document.querySelector('nav ul');
+
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        navUl.classList.add('scrolled');
+      } else {
+        navUl.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return null;
+};
+
+export default ShowNavBackground;
