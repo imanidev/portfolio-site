1aa0a2c4581d97defddf65e404526a8e47cb466e
imani.devs <imaniroberts.dev@gmail.com>
2023-05-25 12:14:52 -0400

    made button to download resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import TypingEffectAbout from '../utils/TypingEffectAbout';
+
+import React from 'react'
+import TypingEffectAbout from '../utils/TypingEffectAbout'
 
 const About = () => {
-  const resumeDoc = 'https://docs.google.com/document/d/1C8bGC4dUdF7Rs_uzVE_G0LVseaJdK-lW/edit?usp=sharing&ouid=110798276445025926264&rtpof=true&sd=true'
-  
-  const openResume = () => {
-    window.open(resumeDoc);
-  };
   return (
     <div>
       <TypingEffectAbout />
-      <div className='bio-container'>
-        <p>I'm not the best at introductions, so I'll get to the point.</p>
-        </div>
-      <button className='resume-btn' onClick={openResume}>Resume</button>
-    
-    </div>
-  );
-};
+       <div className='bio-container'>
+      <p>I'm not the best at introductions, so I'll get to the point.</p>
+      </div>
+      <button className="resume-btn" onClick={() => window.open('/public/assets/Resume/Resume.pdf')}>
+  <i className="resume_download" aria-hidden="true"></i>Resume
+</button>
+      </div>
+  )
+}
 
-export default About;
+export default About
