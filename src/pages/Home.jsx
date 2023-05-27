cb0ba36074b9b3435c2a56dba0ec65bdc9ae3eeb
imani.devs <imaniroberts.dev@gmail.com>
2023-05-27 14:53:46 -0400

    changed web copy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                     With a focus on the MERN stack, I combine creativity, empathy, and a healthy dose of caffeine to solve problems.
                     <br />
                     <br />
-                    From ideation to execution, I prioritize intuitive interfaces and seamless interactions to ensure everyone can easily navigate in the digital world.
+                   Converting concepts into code, I focus on developing user-friendly interfaces and interactions for a better digital experience.
                     </p>
                     </div>
                     <br />
