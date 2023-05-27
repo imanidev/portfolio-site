7cea449955b226004cc9984c765e3e53353ff24f
imani.devs <imaniroberts.dev@gmail.com>
2023-05-27 17:22:59 -0400

    madeproject titles on top

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,13 +6,13 @@ const Projects = () => {
        <ShowNavBackground />
 
             <div className='dictionary-container'>
-                <img className='dict-gif' src='/assets/dict-app.gif' alt='dictionary app' />
-                <p className='desc'>
+                 <p className='desc'>
                   <span className='dict-title'>Dictionary App</span>
                     <br />
                     <br />
                     Technologies: React  • Vite • Bootstrap • API
                 </p>
+                <img className='dict-gif' src='/assets/dict-app.gif' alt='dictionary app' />
                 <br />
                 <button className='demo-btn'>
                     <a href='https://the-simple-dictionary.netlify.app/' target='_blank'>Live Demo</a>
@@ -24,13 +24,13 @@ const Projects = () => {
             <br />
             <br />
             <div className='pong-container'>
-                <img className='pong-gif' src='/assets/pong-app.gif' alt='pong app' />
-                <p className='desc'>
+                 <p className='desc'>
                     <span className='pong-title'>Pong Game</span>
                     <br />
                     <br />
                     Technologies: HTML • CSS  • JavaScript • Canvas API
                 </p>
+                <img className='pong-gif' src='/assets/pong-app.gif' alt='pong app' />
                 <br />
                 <button className='demo-btn'>
                     <a href='https://imanidev.github.io/pong_game/' target='_blank'>Live Demo</a>
